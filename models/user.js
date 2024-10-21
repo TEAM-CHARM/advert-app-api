4e8ba9b7c2c876b9d90c51c12bed90b823ab8d13
charlotte Maanianu Seyab <charlotteseyab19@gmail.com>
2024-10-21 12:04:50 +0000

    made changes

--- models/user.js
+++ models/user.js
@@ -2,17 +2,27 @@ import { Schema, model } from "mongoose";
 
 
 const userSchema = new Schema({
-    name: { type: String,},
+  name: { type: String, },
 
-    email: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
 
-    password: { type: String, required: true },
+  password: { type: String, required: true },
 
-    role: { type: String, enum: ['vendor', 'user'] },
+  role: { type: String, enum: ['vendor', 'user'] },
 
-    bio: { type: String },
+  bio: { type: String },
 
-    profilePic: { type: String },
+  profilePic: { type: String },
+
+  businessName: { type: String },
+
+  businessEmail: { type: String },
+
+  businessPhone: { type: String },
+
+  followers: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+
+
 }, {
   toJSON: {
     versionKey: false, // exclude __v field
